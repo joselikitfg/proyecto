faf35cc26d1e1881b362b2fb601b87abb3c72a5c
Joseliki TFG <joselikitfg@gmail.com>
2024-04-15 18:26:51 +0200

    feat: deploy now depends on tests

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar({ onSearch }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">DescuentApp</a>
+        <a className="navbar-brand" href="/">SmartTrackApp</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>

f470e7de0ac2e3adc814f7ee64eaba62d330a731
Joseliki TFG <joselikitfg@gmail.com>
2024-05-06 16:47:02 +0200

    Feat: cognito login react implementation

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Auth } from 'aws-amplify';
 import Amplify from 'aws-amplify';
-import amplifyConfig from './amplify-config';
 
 Amplify.configure(amplifyConfig);
 

7842247b04faebc8da978f27b3b4e9cd046ab9d7
Joseliki TFG <joselikitfg@gmail.com>
2024-05-14 22:32:15 +0200

    Feat: integration test frontend cypress - search category

---
+++ frontend/cypress/e2e/search_category.cy.js
@@ -0,0 +1,27 @@
+describe('Search Product', () => {
+	beforeEach(()=>{
+        cy.viewport(1920, 1080)
+        Cypress.Keyboard.defaults({
+            keystrokeDelay: 80,
+        })
+        cy.visit('/')
+        cy.get('input[name="username"]').type('cypressuser')
+        cy.get('input[name="password"]').type('Unapassword.123')
+        cy.get('button[type="submit"]').click();
+    })
+
+      
+    it('should actually be accessible', () => {
+        
+        cy.get('input[name="search"]').should('have.attr', 'placeholder', 'Buscar productos')
+        cy.screenshot('contains search button')
+    })
+  
+    // it('should search detergente', () => {
+    //     cy.get('input[name="search"]').type('detergente')
+    //     cy.get('button[type="submit"]').contains('Buscar').click()
+    //     cy.wait(700)
+    //     cy.screenshot('search result')
+    // });
+    
+  })

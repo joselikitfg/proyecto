d904f8e5e34b52d7533e25de79b4c359dcfd31f6
Joseliki TFG <joselikitfg@gmail.com>
2024-05-14 22:33:19 +0200

    Feat: integration test frontend cypress - login

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -1,31 +1,31 @@
-// describe('Login Test', () => {
-//     beforeEach(()=>{
-//         cy.viewport(1920, 1080)
-//         Cypress.Keyboard.defaults({
-//             keystrokeDelay: 80,
-//           })
-//     })
-//     it('Logs in with valid credentials', () => {
+describe('Login Test', () => {
+    beforeEach(()=>{
+        cy.viewport(1920, 1080)
+        Cypress.Keyboard.defaults({
+            keystrokeDelay: 80,
+          })
+    })
+    it('Logs in with valid credentials', () => {
          
-//       cy.visit('/');
-//       cy.screenshot('home-login')
+      cy.visit('/');
+      cy.screenshot('home-login')
 
-//       cy.contains('label', 'Username').should('be.visible')
-//       cy.get('input[name="username"]').should('have.attr', 'placeholder', 'Enter your Username')
-//       cy.contains('label', 'Password').should('be.visible')
-//       cy.get('input[name="password"]').should('have.attr', 'placeholder', 'Enter your Password')
-//       cy.contains('span', 'Sign in').should('be.visible')
-//       cy.wait(700)
-//       cy.get('input[name="username"]').type('cypressuser');
-//       cy.get('input[name="password"]').type('Unapassword.123');
-//       cy.screenshot('home-signin')
-//       cy.screenshot()
-//       cy.wait(700)
-//       cy.get('button[type="submit"]').click();
+      cy.contains('label', 'Username').should('be.visible')
+      cy.get('input[name="username"]').should('have.attr', 'placeholder', 'Enter your Username')
+      cy.contains('label', 'Password').should('be.visible')
+      cy.get('input[name="password"]').should('have.attr', 'placeholder', 'Enter your Password')
+      cy.contains('span', 'Sign in').should('be.visible')
+      cy.wait(700)
+      cy.get('input[name="username"]').type('cypressuser');
+      cy.get('input[name="password"]').type('Unapassword.123');
+      cy.screenshot('home-signin')
+      cy.screenshot()
+      cy.wait(700)
+      cy.get('button[type="submit"]').click();
 
-//       cy.get('.nav-link.active').contains('Sesión iniciada con cypressuser')
-//       cy.wait(700)
-//       cy.screenshot('home')
-//     });
+      cy.get('.nav-link.active').contains('Sesión iniciada con cypressuser')
+      cy.wait(700)
+      cy.screenshot('home')
+    });
     
-// });
+});

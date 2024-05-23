a93bf210bfdad7838e12dd5e9fe0714d773b940d
Joseliki TFG <joselikitfg@gmail.com>
2024-05-23 17:37:53 +0200

    Fix: Adjust dia scrapper to implement into lambda

--- Dia_scrapper.py
+++ Dia_scrapper.py
@@ -2,12 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 import requests
@@ -15,33 +14,6 @@ import json
 from multiprocessing import Process
 import multiprocessing
 import urllib.parse
-
-def scrap_alcampo_image(url):
-
-    response = requests.get(url)
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    script = soup.find('script', {'type': 'application/ld+json'})
-
-
-    if script:
-        data = json.loads(script.string)
-        if 'image' in data and data['image']:
-            product_image_url = data['image'][0]
-        else:
-            product_image_url = None
-
-    else:
-        print('Datos del producto no encontrados.')
-        return None,
-
-    return product_image_url
-
-def scrap_image(product):
-
-    img_url = scrap_alcampo_image(product)
-    return img_url
 
 def modify_url_image(url, new_width):
     parsed_url = urllib.parse.urlparse(url)
@@ -51,6 +23,27 @@ def modify_url_image(url, new_width):
     new_url = parsed_url._replace(query=new_query)
     return urllib.parse.urlunparse(new_url)
 
+def scrap_product_list(driver, products, scraped_product_names):
+    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
+    driver.implicitly_wait(1)
+
+    try:
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//li[@data-test-id='search-product-card-list-item']"))
+        )
+    except TimeoutException:
+        print("TimeoutException: No se pudieron cargar los elementos en el tiempo especificado.")
+        return
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    product_containers = soup.find_all('li', {'data-test-id': 'search-product-card-list-item'})
+
+    print(f'Products to be scrapped in this driver session: {len(product_containers)}')
+
+    for container in product_containers:
+        scrap_one_product(container, scraped_product_names, products)
+
 def scrape_product_details(url):
     options = Options()
     options.add_argument("--headless")
@@ -59,66 +52,64 @@ def scrape_product_details(url):
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+    
     driver.get(url)
-    time.sleep(1)
-    product_elements = driver.find_elements(By.CSS_SELECTOR, "[data-test-id='search-product-card-list-item']")
+    scraped_product_names = set()
     products = []
-
-    for product_element in product_elements:
-        try:
-            image_url = product_element.find_element(By.CSS_SELECTOR, ".search-product-card__product-image").get_attribute('src')
-            modified_image_url = modify_url_image(image_url, '300')
-            name = product_element.find_element(By.CSS_SELECTOR, ".search-product-card__product-name").text
-            try:
-                precio_anterior = product_element.find_element(By.CSS_SELECTOR, ".product-special-offer__strikethrough-price").text
-            except:
-                precio_anterior = None  
-            precio_actual = product_element.find_element(By.CSS_SELECTOR, ".search-product-card__active-price").text
-            product_info = {'image': modified_image_url, 'name': name, 'precio_anterior': precio_anterior, 'precio_actual': precio_actual}
-            products.append(product_info)
-        except NoSuchElementException:
-            print(f"Error procesando el producto {name}")
-    print(len(products))
+    scrap_product_list(driver, products, scraped_product_names)
     driver.quit()
+    print(f'Scrapped Total Items: {len(products)}')
+    print(f'Scrapped: {json.dumps(products, indent=3)}')
     return products
 
+def scrap_one_product(container, scraped_product_names, products):
+    product = {}
+    
+    title_element = container.find('a', {'data-test-id': 'search-product-card-name'})
+    if title_element:
+        product['name'] = title_element.text.strip()
+
+    image_element = container.find('img', {'data-test-id': 'search-product-card-image'})
+    if image_element and 'src' in image_element.attrs:
+        good_image = modify_url_image(image_element['src'], 500)
+        product['image_url'] = 'https://www.dia.es' + good_image
+    else:
+        product['image_url'] = ''
+    
+    price_per_unit_element = container.find('p', {'data-test-id': 'search-product-card-kilo-price'})
+    if price_per_unit_element:
+        product['price_per_unit'] = price_per_unit_element.text.strip()
+
+    total_price_element = container.find('p', {'data-test-id': 'search-product-card-unit-price'})
+    if total_price_element:
+        product['total_price'] = total_price_element.text.strip()
+
+    if product.get('name') and product['name'] not in scraped_product_names:
+        products.append(product)
+        scraped_product_names.add(product['name'])
 
 def save_product_to_json(product, json_file='products2.json'):
     data = []  
-
-
     try:
         with open(json_file, 'r', encoding='utf-8') as file:
             file_contents = file.read()
             if file_contents:  
                 data = json.loads(file_contents)
     except FileNotFoundError:
-
         pass
     except json.JSONDecodeError:
-
         print(f"Warning: {json_file} contains invalid JSON. Overwriting with new data.")
 
-
     data.append(product)
-
 
     with open(json_file, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
 
     print(f"Product '{product['name']}' saved to {json_file}")
-    
-
 
 def generate_urls(categories, base_url="https://www.dia.es/search?q={category}"):
-    """
-    Genera una lista de URLs para las categorías dadas.
-    :param categories: Lista de categorías (números al final de la URL).
-    :return: Lista de URLs completas.
-    """
     urls = [base_url.replace("{category}", str(category)) for category in categories]
     return urls
-
 
 def scrap_product_by_category(url):
     products = None
@@ -135,17 +126,12 @@ def scrap_product_by_category(url):
         print(f"Datos no válidos para la URL {url}, omitiendo...")         
 
 procs = [] 
-
-names = ["Aceitunas"]#,"Frutas","Verduras","Pan","Cereales","Hortalizas","Harina","Quesos","Legumbres","Pasta","Aceite"]
-
+names = ["Aceitunas"]
 url_base = "https://www.dia.es/search?q={category}"
 generated_urls = generate_urls(names, url_base)
 for url in generated_urls:
-    #scrap_product_by_category(url)
-    # print(url)
     proc = Process(target=scrap_product_by_category, args=(url,))
     procs.append(proc)
-
 
 for proc in procs:
     print("Lanzando proceso")
@@ -153,6 +139,3 @@ for proc in procs:
 
 for proc in procs:
     proc.join()
-            
-
-
